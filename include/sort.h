035c202581d2d3b213146244945a2dcca84b1fa3
qianguozheng <guozhengqian0825@126.com>
2013-10-24 22:54:23 +0800

    update header file macro for better maintain

--- include/sort.h
+++ include/sort.h
@@ -1,7 +1,6 @@
-#include <stdio.h>
-
 #ifndef __SORT_H_
 #define __SORT_H_
+#include <stdio.h>
 
 int bubble(int *arr, int size);
 int heapsort(int *arr, int size);
--- include/util.h
+++ include/util.h
@@ -1,4 +1,9 @@
 #include <stdio.h>
 
+#ifndef __UTIL_H_
+#define __UTIL_H_
+
 void print(int *arr, int size);
 int swap(int *a, int *b);
+
+#endif
