c3c468d45cd784a76f2ed9bf4dff941e9eeb5c1c
richard <richard@focalcrest.com>
2016-02-01 22:35:18 +0800

    Add passwd interactive change demo.

---
+++ util/passwdchg.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int execute_cmd()
+{
+	int fd[2];
+	int fd2[2];
+	pid_t pid;
+	
+	char buf[100] = {'\0'};
+	char tmp[100] = {'\0'};
+	pipe(fd);
+	pipe(fd2);
+	
+	if ((pid = fork()) == -1)
+	{
+		printf("Fork failed\n");
+		exit(-1);
+	}
+	
+	fprintf(stderr, "Execute cmd\n");
+	if (pid == 0)
+	{
+		//child process
+		close(fd[0]);
+		close(fd2[1]);
+		dup2(fd[1], 1);
+		dup2(fd2[0], 0);
+		fprintf(stderr, "Child:\n");
+		//execlp("passwd", NULL);
+		execlp("/usr/bin/passwd", "passwd", "root", NULL);
+		fprintf(stderr, "Child2:\n");
+	}
+	else if (pid > 0)
+	{
+		//parent process
+		close(fd[1]);
+		close(fd2[0]);
+		dup2(fd[0], 0);
+		dup2(fd2[1], 1);
+		
+		fprintf(stderr, "Parent:\n");
+		read(1, buf, sizeof(buf));
+		fprintf(stderr, "buf=%s\n", buf);
+		sleep(1);
+		read(1, buf, sizeof(buf));
+		fprintf(stderr, "buf=%s\n", buf);
+		sleep(1);
+		read(1, buf, sizeof(buf));
+		fprintf(stderr, "buf=%s\n", buf);
+		sleep(1);
+		read(1, buf, sizeof(buf));
+		fprintf(stderr, "buf=%s\n", buf);
+	}
+	
+	return 0;
+}
+
+int main(void)
+{
+	execute_cmd();
+}
