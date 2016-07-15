4a96d2a112264545992abdc47fde4f60fdd896a4
qianguozheng <guozhengqian0825@126.com>
2016-07-15 18:20:06 +0800

    Add udpate script to udpate shadow password

---
+++ python/shadowupdate.sh
@@ -0,0 +1,16 @@
+while true
+
+do
+
+wget -q http://hiweeds.net/shadowsocks.json -P /tmp/
+newMd5=$(md5sum /tmp/shadowsocks.json | cut -d ' ' -f1)
+oldMd5=$(md5sum /etc/shadowsocks.json | cut -d ' ' -f1)
+
+if [ "$newMd5" != "$oldMd5" ]; then
+	mv /tmp/shadowsocks.json /etc/shadowsocks.json
+	killall -9 ss-redir
+	/etc/init.d/shadowsocks start
+fi
+
+sleep 120
+done
